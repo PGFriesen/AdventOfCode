abb3dc371584ed7d4561a35c689c4c506d916b47
PGFriesen <paulgfriesen@gmail.com>
2021-12-04 22:26:18 -0500

    day 4 and day3 part 1

--- 2021/day3.py
+++ 2021/day3.py
@@ -17,21 +17,95 @@ Returns:
  - strings: The list of values as strings
 """
 def readFile(input_file):
+    input_file = "testInput.txt"
     with open(input_file,'r') as f:
         lines = f.readlines()
-        strings = [i.strip(' \n') for i in lines]
-        try:
-            integers = [int(i.strip(' \n')) for i in lines]
-        except:
-            print("Input values are not integers")
-            integers = strings
+        input_list = [i.strip(' \n') for i in lines]
         
-    return integers, strings
+    return input_list
 
 def partOne(arg):
-    print("Answer to Part 1: ")
+    
+    str_gamma, str_epsilon = "",""
+    for bit in range(len(arg[0])):
+        z, o = 0,0
+
+        # this loop counts the number of 0's and 1's in the given column
+        for row, value in enumerate(arg):
+            if arg[row][bit] == '1':
+                o += 1
+            else:
+                z += 1
+
+        # determine the most common bits (gamma) and least common bits (epsilon)
+        if o >= z:
+            str_gamma += '1'
+            str_epsilon += '0'
+        elif o < z:
+            str_gamma += '0'
+            str_epsilon += '1'
+
+    print("\nPart 1:\ngamma = %s (%d)\nepsilon = %s (%d)\nPower Consumption: %d\n\n" % (str_gamma, int(str_gamma, 2), str_epsilon, int(str_epsilon, 2), int(str_epsilon,2) * int(str_gamma, 2)))
+
+    return str_gamma, str_epsilon
 
 def partTwo(arg):
+
+    gamma, epsilon = partOne(arg)
+    gen_ratings, scr_ratings = arg.copy(),arg.copy()
+    g,s = "",""
+
+    print("gamma=%s" % (gamma))
+    for i in range(len(gamma)):
+        found = 0
+        substr = gamma[0:i+1]
+        print("substring=%s" % (substr))
+        for val in arg:
+            if val[0:i+1] == substr:
+                print(val)
+                found += 1
+        
+
+    print("\n\nepsilon=%s" % (epsilon))
+    for i in range(len(epsilon)):
+        found = 0
+        substr = epsilon[0:i+1]
+
+        for val in arg:
+            if val[0:i+1] == substr:
+                found += 1
+
+    """
+    #for index, bit in enumerate(gamma):
+    index = 0
+    while(len(gen_ratings) > 1):    
+        bit = gamma[index]
+        
+        for value in arg:
+            if len(gen_ratings) > 1 and value in gen_ratings and value[index] != gamma[index]:
+                gen_ratings.remove(value)
+        #print(gamma)
+        #print(index, bit)
+        for val in gen_ratings:
+            print(val)
+
+        index += 1          
+        #print("\n\n")
+
+    index = 0
+    while(len(scr_ratings) > 1):
+        bit = epsilon[index]
+
+        for value in arg:
+            if len(scr_ratings) > 1 and value in scr_ratings and value[index] != epsilon[index]:
+                scr_ratings.remove(value)
+        #print(epsilon)
+        #print(index, bit)
+        for val in scr_ratings:
+            print(val)
+        #index += 1
+        print("\n\n")
+        """
     print("Answer to Part 2: ")
 
 """
@@ -42,10 +116,9 @@ Each day in December has it's own python script and it's own input file, so they
 def main(arguments):
     inputFile = "input" + arguments[0][3:arguments[0].index('.')] + ".txt"
 
-    integer_input, string_input = readFile(inputFile)
+    input_values = readFile(inputFile)
     
-    partOne(integer_input)
-    partTwo(integer_input)
+    partTwo(input_values)
     
 if __name__ == '__main__':
     sys.exit(main(sys.argv[0:]))
--- 2021/day4.py
+++ 2021/day4.py
@@ -3,49 +3,91 @@
 import os
 import sys
 
-
 """
-This function reads in from the input_file as a list where each line is a single item in the list
-
-If all of the input values are numeric, then integers will contain....integers (otherwise integers and strings point to the same list)
-
-Param: 
- - input_file: The filename to read from
-
-Returns:
- - integers: The list of values. If the values from input_file are not numeric, this will simply be the same as strings
- - strings: The list of values as strings
+"Zhu Li, do the thing!"
+"Yes Varrick"
 """
-def readFile(input_file):
+def pythonDoTheThing(input_file):
     with open(input_file,'r') as f:
         lines = f.readlines()
-        strings = [i.strip(' \n') for i in lines]
-        try:
-            integers = [int(i.strip(' \n')) for i in lines]
-        except:
-            print("Input values are not integers")
-            integers = strings
+    
+    # Take the input file and organize it into the list of bingo cards and the deck of numbers for the announcer to draw from
+    number_deck, bingo_cards = lines[0].split(','), []
+    for i in range(1, len(lines), 6):
+        bingo_card = []
+        for k in range(1,6):
+            bingo_card.append(lines[i+k].strip().split())
+        bingo_cards.append(bingo_card)
+
+    score = playBingo(number_deck, bingo_cards)
+    #print("Answer = %d" % (score))
+
+"""
+Draw a number from the deck and mark each card, and the game ends when a winning card is found
+"""
+def playBingo(deck, card_list):
+
+    winners = []
+    winningNum = 0
+    # Announcer draws a number
+    for turn, number in enumerate(deck):
+
+        # Check each bingo card
+        for card_num, card in enumerate(card_list):
+            if card_num not in winners: # for part 2, we want to find the last player to win (which was card #71)
+                markCard(number, card) # mark the card
+                if bingo(card): # check if the card won
+                    winners.append(card_num)
+                    winningNum = int(number)
+    
+    print(calculateCardScore(card_list[winners[-1]]) * int(winningNum)) 
+
+"""
+Provided a number and a bingo card, if the number exists on the card, replace it with a mark 'M'
+"""
+def markCard(num, bingo_card):
+    # Check each row for the number and replace it with an 'M' if found
+    for row in bingo_card:
+        if row.count(num) > 0:
+            row[row.index(num)] = 'M'
+
+"""
+Check a bingo_card to see if it won
+"""
+def bingo(bingo_card): 
+    
+    for row in range(5):
+        # count the whole row for the number of 'M' marks
+        if bingo_card[row].count('M') == 5:
+            return True
+       
+        # check the whole column
+        won = True
+        for col in range(5):
+            if bingo_card[col][row] != 'M':
+                won = False
         
-    return integers, strings
+        if won:
+            return True
 
-def partOne(arg):
-    print("Answer to Part 1: ")
-
-def partTwo(arg):
-    print("Answer to Part 2: ")
-
+    # If we didn't find a winning row or col, we don't have bingo
+    return False
+        
 """
-Main function to run the program. The file to read input from is determined by the name of the executable script.
-
-Each day in December has it's own python script and it's own input file, so they are named accordingly - "day1.py" uses "input1.txt"
+Once a winner is found, count up all non-marked numbers and return the score
 """
+def calculateCardScore(bingo_card):
+    score = 0
+    for row in bingo_card:
+        for num in row:
+            if num != 'M':
+                score += int(num)
+
+    return score
+
 def main(arguments):
-    inputFile = "input" + arguments[0][3:arguments[0].index('.')] + ".txt"
+    input_file = "input" + arguments[0][3:arguments[0].index('.')] + ".txt"
+    pythonDoTheThing(input_file)
 
-    integer_input, string_input = readFile(inputFile)
-    
-    partOne(integer_input)
-    partTwo(integer_input)
-    
 if __name__ == '__main__':
     sys.exit(main(sys.argv[0:]))
